56adfb859287fef0763c73934076261a1b490980
MatheusTI1 <matheussouzati1@gmail.com>
2023-05-13 23:56:11 -0300

    finalização tipos variaveis

--- Primeiro_Projeto_Java/src/edu/matheus/Tipos_Variáveis/TiposVariaveis.java
+++ Primeiro_Projeto_Java/src/edu/matheus/Tipos_Variáveis/TiposVariaveis.java
@@ -1,5 +1,20 @@
 package edu.matheus.Tipos_Variáveis;
 
 public class TiposVariaveis {
-    
+    public static void main(String[] args) {
+        double salarioMinimo = 2500.33; //dois mil e quinhentos reais e trinta e três centavos ou dois mil e quinhentos virgula trinta e três centavos
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+        
+    }
+
 }

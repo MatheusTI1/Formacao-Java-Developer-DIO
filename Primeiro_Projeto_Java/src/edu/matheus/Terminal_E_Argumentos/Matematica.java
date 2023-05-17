dce42f2b8e20747db2d9d9d9550db10da46d60a2
MatheusTI1 <matheussouzati1@gmail.com>
2023-05-17 03:52:59 -0300

    Aprendendo sintaxe completo

---
+++ Primeiro_Projeto_Java/src/edu/matheus/Terminal_E_Argumentos/Matematica.java
@@ -0,0 +1,19 @@
+package edu.matheus.Terminal_E_Argumentos;
+import java.util.Scanner;
+
+public class Matematica {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double A, B, media;
+
+        A = sc.nextDouble();
+        B = sc.nextDouble();
+
+ //TODO: Complete os espaços em branco com uma possível solução para o desafio 
+        media = ( A * 3.5 + B * 7.5)/11; 
+
+        System.out.printf("MEDIA = %.5f", + media     );
+        System.out.println();
+    }
+}
---
+++ Primeiro_Projeto_Java/src/edu/matheus/Terminal_E_Argumentos/Terminal.java
@@ -0,0 +1,7 @@
+package edu.matheus.Terminal_E_Argumentos;
+
+public class Terminal {
+    public static void main(String[] args) {
+        System.out.println("Oi, fui executado pelo Terminal");
+    }
+}

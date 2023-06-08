3c517a427a91630651db1b14bd5d2663dc58cf41
MatheusTI1 <matheussouzati1@gmail.com>
2023-06-07 23:29:19 -0300

    Classes

---
+++ Fundamentos_da_Programação_Orientada_a_Objetos_com_Java/src/School.java
@@ -0,0 +1,24 @@
+public class School {
+// Criando objetos a partir da classe Student
+
+// Objetos:
+
+    public static void main(String[] args) throws Exception {
+      Student student1 = new Student();
+      student1.name= "John";
+      student1.age= 12;
+   
+
+      Student student2 = new Student();
+      student2.name= "Sophia";
+      student2.age= 10;
+
+
+      Student student3 = new Student();
+      student3.name= "Lily";
+      student3.age= 11;
+
+      
+
+    }
+}
---
+++ Fundamentos_da_Programação_Orientada_a_Objetos_com_Java/src/Student.java
@@ -0,0 +1,22 @@
+public class Student {
+    // Criar molde, logo depois constroi os objetos com os moldes predefinidos
+
+    //Molde:
+    String name;
+    int age;
+
+    void eating (){
+        System.out.println("eating lunch");
+    }
+
+    void drinking (){
+        System.out.println("drinking juice");
+    }
+
+    void running (){
+        System.out.println("correndo");
+    }
+    
+}
+
+

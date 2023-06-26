17fa77cbe520b98086d68f1cd3b60fee0365404a
MatheusTI1 <matheussouzati1@gmail.com>
2023-06-26 17:46:24 -0300

    abstração

---
+++ Abstração/src/Computador.java
@@ -0,0 +1,29 @@
+public class Computador {
+    public static void main(String[] args) {
+        // Abrindo Telegram
+        System.out.println("Telegram");        
+        Telegram tlg = new Telegram();
+        tlg.enviarMensagem();
+        tlg.receberMensagem();
+
+        System.out.println("\nMSN");
+        Msn msn = new Msn();
+        msn.enviarMensagem();
+        msn.receberMensagem();
+
+        System.out.println("\nFacebook");
+        Facebook fcb = new Facebook();
+        fcb.enviarMensagem();
+        fcb.receberMensagem();
+        
+
+        // Sem o encapsulamento o usuário tera que fazer todas essas ações toda vez que enviar a mensagem de forma manual
+        // msn.validarConectadoInternet();
+        // msn.enviarMensagem();
+        // msn.savlarHistoricoMensagem();
+        // msn.receberMensagem();
+
+        // já com o encapsulamento o toda vez que ele disparar uma mensagem todas as ações serão efetuadas de forma automática sem que o usuário se quer veja o processo que levou
+
+    }
+}
